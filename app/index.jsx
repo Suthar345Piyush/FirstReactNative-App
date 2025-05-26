47ddd3580bb613970047382468acad1c6e8a1b3f
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-05-26 10:43:52 +0530

    learning

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View  , Image} from 'react-native';
+import Logo from '../assets/img/logo_light.png';
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+     <Image source={Logo} style={styles.img}/>
+      <Text style={[styles.title , {color : 'orange'}]}>The Number 1</Text>
+
+
+      <Text style={{marginBottom : 30 , marginTop : 10}}>Reading List App</Text>
+
+      
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container : {
+       flex : 1,
+       alignItems : 'center',
+       justifyContent : 'center'
+    },
+
+    title : {
+       fontWeight : 'bold',
+       fontSize : 18
+    },
+
+   
+
+    img : {
+       marginVertical : 20,
+    }
+})
 
 
