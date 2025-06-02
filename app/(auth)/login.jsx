6c31ae6561d4ecff9f58d63b025b957a733835fa
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-06-02 23:21:12 +0530

    button like pressable components

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet , Pressable , Text} from 'react-native'
 import { Link } from 'expo-router'
 
 
@@ -6,9 +6,18 @@ import { Link } from 'expo-router'
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import { Colors } from '../../constants/Colors';
+import ThemedButton from '../../components/ThemedButton';
 
 
 const Login = () => {
+
+  const handleSubmit = () => {
+     console.log("Login form submitted");
+  }
+
+
+
   return (
       <ThemedView style={styles.container}>
       <Spacer />
@@ -16,6 +25,16 @@ const Login = () => {
     <ThemedText title={true} style={styles.title}>
        Login to Your Account
     </ThemedText>
+ 
+   {/* <Pressable onPress={handleSubmit}  style={({pressed}) => [styles.btn , pressed && styles.pressed]}> 
+     <Text style={{color : '#f2f2f2'}}>Login</Text>
+   </Pressable> */}
+
+   <ThemedButton onPress={handleSubmit}>
+     <Text style={{ color: 'white'}}>Login</Text>
+   </ThemedButton>
+
+
 
     <Spacer height={100}/>
     <Link href="/register">
@@ -32,13 +51,26 @@ export default Login
 const styles = StyleSheet.create({
      container : {
        flex : 1,
-       justifyContent : "center"
+       justifyContent : "center",
+       alignItems : 'center'
      },
 
      title : {
        textAlign : "center",
        fontSize : 18,
        marginBottom : 30
+     },
+
+     btn : {
+       backgroundColor : Colors.primary,
+       padding : 15,
+       borderRadius : 5,
+     },
+
+     pressed : {
+       opacity : 0.8
      }
 });
+
+
 
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,13 +1,22 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet  , Text} from 'react-native'
 import { Link } from 'expo-router'
 
 
 import ThemedView from '../../components/ThemedView';
 import ThemedText from '../../components/ThemedText';
+import ThemedButton from '../../components/ThemedButton';
 import Spacer from '../../components/Spacer';
+import { Colors } from '../../constants/Colors';
 
 
 const register = () => {
+
+  
+   const handleSubmit = () => {
+      console.log("Register form submitted");
+   }
+
+
   return (
 
       <ThemedView style={styles.container}>
@@ -17,6 +26,11 @@ const register = () => {
           <ThemedText title={true} style={styles.title}>
                Register a Account             
           </ThemedText>
+
+          <ThemedButton onPress={handleSubmit}>
+     <Text style={{ color: 'white'}}>Register</Text>
+   </ThemedButton>
+
 
 
           <Spacer height={100} />
@@ -38,13 +52,24 @@ const styles = StyleSheet.create({
    
    container : {
      flex : 1,
-     justifyContent : "center"
+     justifyContent : "center",
+     alignItems : 'center'
    },
 
    title : {
       textAlign : "center",
       fontSize : 18,
       marginBottom : 30
+   },
+
+   pressed : {
+       opacity : 0.0
+   },
+
+   btn : {
+      backgroundColor : Colors.primary,
+      padding : 15,
+      borderRadius : 5
    }
 });
 
